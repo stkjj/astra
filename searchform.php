18559469b84ef457e6e945809e4afe09c829829f
Fahim Murshed <fahimshaper@gmail.com>
2021-06-15 13:58:15 +0600

    Update searchform.php
    Bug in searchform.php translation

--- searchform.php
+++ searchform.php
@@ -21,6 +21,6 @@
 			</button>
 		<?php } ?>
 	</label>
-	<input type="submit" class="search-submit" value="Search">
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'astra' ); ?>" />
 </form>
 <?php
