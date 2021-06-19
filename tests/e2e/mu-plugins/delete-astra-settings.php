ef4bb2417f51a9f81888e141e789edb09bbc6d3e
Nikhil Chavan <email@nikhilchavan.com>
2021-06-19 20:13:17 +0530

    Setup e2e test enviornment
    Add base jest config, Add mu-plugin to reset the site in between the tests

---
+++ tests/e2e/mu-plugins/delete-astra-settings.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name: REST Delete Astra Settings.
+ * Plugin URI:  https://github.com/brainstormforce/astra
+ * Description: Plugin to add REST Endpoint to delete Astra Settings.
+ * Author:      Brainstorm Force
+ * Author URI:  https://brainstormforce.com
+ */
+
+namespace Astra\REST\Settings;
+
+use WP_Rest_Server;
+
+const REST_NAMESPACE = 'astra/v1';
+const REST_BASE = 'e2e-utils';
+
+function bootstrap() : void {
+	add_action( 'rest_api_init', __NAMESPACE__ . '\\rest_route' );
+}
+
+function rest_route() : void {
+	register_rest_route(
+		REST_NAMESPACE, REST_BASE . '/reset-theme', array(
+			array(
+				'methods' => WP_Rest_Server::DELETABLE,
+				'callback' => function () {
+					delete_option( 'astra-settings' );
+					remove_theme_mod( 'custom_logo' );
+					delete_option( 'site_title' );
+					delete_option( 'site_icon' );
+					update_option( 'blogdescription', 'Astra Test Enviornment' );
+
+					return rest_ensure_response([
+						'success' => true
+					]);
+				},
+				'permission_callback' => '__return_true',
+			),
+		)
+	);
+}
