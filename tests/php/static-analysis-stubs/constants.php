ad173ffb85cfae3dc68bf96de6465903d195efbc
Nikhil Chavan <email@nikhilchavan.com>
2021-06-22 08:53:53 +0530

    Supress errors from stubs and errors relates type hinting

--- tests/php/static-analysis-stubs/constants.php
+++ tests/php/static-analysis-stubs/constants.php
@@ -9,3 +9,4 @@ define( 'ASTRA_HEADER_SEARCH_URI', '' );
 define( 'ASTRA_THEME_SETTINGS', '' );
 define( 'ASTRA_THEME_VERSION', '' );
 define( 'ASTRA_RELATED_POSTS_DIR', '' );
+define( 'ASTRA_THEME_BREADCRUMBS_DIR', '' );
