4257e2517bdd6e2a03e7400048f39a63f8805ead
imnavanath <navanathb@bsf.io>
2021-06-30 15:38:16 +0530

    improvement: Added add_theme_support for rankmath breakcrumb

---
+++ inc/compatibility/class-astra-rank-math.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Rank Math Compatibility File.
+ *
+ * @package Astra
+ * @since x.x.x
+ */
+
+// If plugin - 'RankMath' not exist then return.
+if ( ! class_exists( 'RankMath' ) ) {
+	return;
+}
+
+/**
+ * Astra Rank Math Compatibility
+ *
+ * @since x.x.x
+ */
+class Astra_Rank_Math {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'web_stories_setup' ) );
+	}
+
+	/**
+	 * Add theme support for Rankmath.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function web_stories_setup() {
+		add_theme_support( 'rank-math-breadcrumbs' );
+	}
+}
+
+/**
+ * Kicking this off by 'new' object.
+ */
+new Astra_Rank_Math();
