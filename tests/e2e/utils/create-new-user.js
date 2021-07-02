04f688b277bd5a36430523654e3b3d2fa82643d9
premanshup <premanshup@bsf.io>
2021-07-02 13:08:29 +0530

    Added new functions to Create New User and Delete create user in E2E testing.

--- tests/e2e/utils/create-new-user.js
+++ tests/e2e/utils/create-new-user.js
@@ -1,15 +1,35 @@
-import fetch from 'node-fetch';
-import { createURL } from '@wordpress/e2e-test-utils';
+ /**
+  * Internal dependencies
+  */
+ import { switchUserToAdmin } from '@wordpress/e2e-test-utils';
+ import { switchUserToTest } from '@wordpress/e2e-test-utils';
+ import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const createNewUser = async ( data ) => {
-	return await fetch(
-		createURL( '/wp-json/astra/v1/e2e-utils/set-astra-settings' ),
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify( { settings: data } ),
-		},
-	);
-};
+/**
+ * Create a new user account.
+ *
+ * @param {string}  username  User name.
+ * @param {string?} firstName First name.
+ * @param {string?} lastName  Larst name.
+ */
+ export async function createUser( username, firstName, lastName ) {
+	 console.log(username);
+	await switchUserToAdmin();
+	await visitAdminPage( 'user-new.php' );
+
+	await page.type( '#user_login', username );
+	await page.type( '#email', username + '@example.com' );
+	if ( firstName ) {
+		await page.type( '#first_name', firstName );
+	}
+	if ( lastName ) {
+		await page.type( '#last_name', lastName );
+	}
+	await page.click( '#send_user_notification' );
+
+	await Promise.all( [
+		page.click( '#createusersub' ),
+		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	] );
+	await switchUserToTest();
+}
---
+++ tests/e2e/utils/delete-user.js
@@ -0,0 +1,44 @@
+/**
+ * Internal dependencies
+ */
+ import { switchUserToAdmin } from '@wordpress/e2e-test-utils';
+ import { switchUserToTest } from '@wordpress/e2e-test-utils';
+ import { visitAdminPage } from '@wordpress/e2e-test-utils';
+
+ /**
+  * Delete a user account.
+  *
+  * @param {string} username User name.
+  */
+ export async function deleteUser( username ) {
+	 await switchUserToAdmin();
+	 await visitAdminPage( 'users.php' );
+
+	 const [ userLink ] = await page.$x(
+		 `//td[@data-colname="Username"]//a[contains(text(), "${ username }")]`
+	 );
+
+	 if ( ! userLink ) {
+		 await switchUserToTest();
+		 return;
+	 }
+
+	 // Focus to unveil actions
+	 await userLink.focus();
+
+	 // Tab twice to focus 'Delete'
+	 await page.keyboard.press( 'Tab' );
+	 await page.keyboard.press( 'Tab' );
+
+	 await Promise.all( [
+		 page.keyboard.press( 'Enter' ),
+		 page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	 ] );
+
+	 // Confirm
+	 await Promise.all( [
+		 page.click( 'input#submit' ),
+		 page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	 ] );
+	 await switchUserToTest();
+ }
