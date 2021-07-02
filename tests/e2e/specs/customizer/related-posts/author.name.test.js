1b3aa17b819519a7f207fbc4b3d0e45f7b7ababf
premanshup <premanshup@bsf.io>
2021-07-02 20:06:20 +0530

    Updated select author logic from edit posts to assign author while publishing the post.

--- tests/e2e/specs/customizer/related-posts/author.name.test.js
+++ tests/e2e/specs/customizer/related-posts/author.name.test.js
@@ -3,11 +3,9 @@ import {
 	createNewPost,
 	publishPost,
 	visitAdminPage,
-	trashAllPosts,
 } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/set-customize';
 import { createUser } from '../../../utils/create-new-user';
-import { deleteUser } from '../../../utils/delete-user';
 
 describe( 'Related Posts correct Author Name', () => {
 	it( 'Related Posts should display correct author name', async () => {
@@ -27,53 +25,18 @@ describe( 'Related Posts correct Author Name', () => {
 
 		// Create New Post.
 		await createNewPost( { postType: 'post', title: 'Related Post - adminRelatedPost' } );
+		// Select new user for publishing the post.
+		await page.waitForSelector( '#inspector-select-control-3' );
+
+		const option = (await page.$x(
+			'//*[@id = "inspector-select-control-3"]/option[text() = "adminRelatedPost"]'
+		))[0];
+
+		const value = await (await option.getProperty('value')).jsonValue();
+
+		await page.select('#inspector-select-control-3', value);
+
 		await publishPost();
-		await visitAdminPage( 'edit.php' );
-
-		const title = 'adminRelatedPost';
-
-		const [ postLink ] = await page.$x(
-			`//td[@data-colname="Title"]//a[contains(text(), "${ title }")]`
-		);
-
-		if ( ! postLink ) {
-			await switchUserToTest();
-			return;
-		}
-
-		// Focus to unveil actions
-		await postLink.focus();
-
-		// Tab twice to focus 'Edit'
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-
-		await page.keyboard.press( 'Enter' );
-
-		// Tab twice to focus 'Edit'
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-
-		await page.keyboard.press( 'ArrowDown' );
-
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-
-		// Set Post Author to New User - adminRelatedPost.
-		await page.keyboard.press( 'Enter' );
 
 		await page.goto( createURL( '/related-post-admin/' ), {
 			waitUntil: 'networkidle0',
