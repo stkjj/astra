60a8ca5e16e94b3aa32d559efc125ccdd28a5e49
Nikhil Chavan <email@nikhilchavan.com>
2021-07-14 16:51:04 +0530

    cs fixes

--- bin/scripts/dynamic-css-to-file.js
+++ bin/scripts/dynamic-css-to-file.js
@@ -1,14 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const puppeteer = require( 'puppeteer-core' );
 const fs = require( 'fs' );
 
 const url = process.argv[ 2 ];
 const stylesheetId = process.argv[ 3 ];
 
-async function getCssFromUrl( url, stylesheetId ) {
+async function getCssFromUrl( pageUrl, stylesheetSelector ) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto( url );
-	const inlineCss = await page.$eval( stylesheetId, ( el ) => el.innerText );
+	await page.goto( pageUrl );
+	const inlineCss = await page.$eval( stylesheetSelector, ( el ) => el.innerText );
 	await browser.close();
 
 	fs.writeFileSync( 'assets/dynamic-css.css', inlineCss.replace( /(^[ \t]*\n)/gm, '' ) );
