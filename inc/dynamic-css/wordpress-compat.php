54e1b5f65fb79fc48398e6e2452c6dc2317acc1a
imnavanath <navanathb@bsf.io>
2021-07-15 15:23:33 +0530

    refactor: Adding search block default styling WIP

--- inc/dynamic-css/wordpress-compat.php
+++ inc/dynamic-css/wordpress-compat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Astra WordPress compatibility - Dynamic CSS.
+ * Astra WordPress-5.8 compatibility - Dynamic CSS.
  *
  * @package astra
  * @since x.x.x
@@ -23,15 +23,23 @@ function astra_wordpress_compat_css( $dynamic_css ) {
 	if ( Astra_Dynamic_CSS::is_wordpress_5_8_support_enabled() ) {
 
 		$desktop_css = '
-        form.wp-block-search .wp-block-search__input, .wp-block-search.wp-block-search__button-inside .wp-block-search__inside-wrapper {
+		.wp-block-search {
+			margin-bottom: 20px;
+		}
+        form.wp-block-search .wp-block-search__input, .wp-block-search.wp-block-search__button-inside .wp-block-search__inside-wrapper, .site-content .wp-block-search.wp-block-search__button-inside .wp-block-search__inside-wrapper {
             border-color: #eaeaea;
 			background: #fafafa;
         }
+		.site-content .wp-block-search.wp-block-search__button-inside .wp-block-search__inside-wrapper .wp-block-search__input:focus {
+			outline: thin dotted;
+		}
 		.site-content form.wp-block-search .wp-block-search__inside-wrapper .wp-block-search__input {
-			padding: 15px;
+			padding: 12px;
 		}
 		.wp-block-search__button svg {
             fill: currentColor;
+			width: 20px;
+			height: 20px;
         }';
 
 		return $dynamic_css .= Astra_Enqueue_Scripts::trim_css( $desktop_css );
