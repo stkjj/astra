ba2e36f546adab360a070f6d8f966baea5b0ebcf
Nikhil <Nikschavan@users.noreply.github.com>
2021-07-20 10:45:12 +0530

    cs fix - Remove unnecessary quotes

--- tests/e2e/specs/customizer/header-builder/Site-identity/site-tagline.test.js
+++ tests/e2e/specs/customizer/header-builder/Site-identity/site-tagline.test.js
@@ -5,18 +5,18 @@ describe( 'Site Tagline Typography settings and color settings in the customizer
 	it( 'site tagline typography and color should apply corectly', async () => {
 		const siteTagline = {
 			'display-site-tagline-responsive': {
-				'desktop': true,
-				'tablet': true,
-				'mobile': true,
+				desktop: true,
+				tablet: true,
+				mobile: true,
 			},
 			'body-font-family': 'Raleway, sans-serif',
 			'body-font-variant': '800',
 			'body-font-weight': '800',
 			'body-text-transform': 'uppercase',
 			'font-size-site-tagline': {
-				'desktop': 22,
-				'tablet': 20,
-				'mobile': 18,
+				desktop: 22,
+				tablet: 20,
+				mobile: 18,
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
