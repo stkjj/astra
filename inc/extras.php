a79fabb3e8accfb51c7d91de8f2b844d392d65a8
imnavanath <navanathb@bsf.io>
2021-08-05 19:42:24 +0530

    refactor: Avoided astra_get_option call

--- inc/extras.php
+++ inc/extras.php
@@ -698,17 +698,17 @@ function astra_can_remove_elementor_toc_margin_space() {
  */
 function astra_remove_widget_design_options() {
 	$astra_settings                                 = get_option( ASTRA_THEME_SETTINGS );
-	$astra_settings['remove-widget-design-options'] = isset( $astra_settings['remove-widget-design-options'] ) ? false : true;
+	$remove_widget_design_options = isset( $astra_settings['remove-widget-design-options'] ) ? false : true;
 
 	// True -> Hide widget sections, False -> Display widget sections.
 	$is_widget_design_sections_hidden = true;
 
-	if ( ! $astra_settings['remove-widget-design-options'] ) {
+	if ( ! $remove_widget_design_options ) {
 		// For old users we will show widget design options by anyways.
 		$is_widget_design_sections_hidden = false;
 	} else {
 		// Considering the user is new now.
-		if ( astra_get_option( 'remove-widget-design-options' ) ) {
+		if ( isset( $astra_settings['remove-widget-design-options'] ) && $astra_settings['remove-widget-design-options'] ) {
 			// User was on WP-5.8 lesser version previously and he may update their WordPress to 5.8 in future. So we display the options in this case.
 			$is_widget_design_sections_hidden = false;
 		} elseif ( astra_has_widgets_block_editor() ) {
